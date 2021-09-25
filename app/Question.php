f42f21a1e93ec5de73038026f36e826611190968
Richard Onyango <akellorich@yahoo.com>
2021-09-25 18:03:07 +0300

    Added author and date created

--- app/Question.php
+++ app/Question.php
@@ -12,8 +12,17 @@ class Question extends Model
         return $this->belongsTo(User::class);
     }
 
-    function setTitleAttribute($value){
+    public function setTitleAttribute($value){
         $this->attributes['title']=$value;
         $this->attributes['slug']=str_slug($value);
     }
+
+    public function getUrlAttribute(){
+        return route ("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
 }
