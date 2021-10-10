5ea43e7db74d4c958c0739ca6e60b47ef96d3007
Richard Onyango <akellorich@yahoo.com>
2021-10-10 17:35:10 +0300

    Added vote controls to questions

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,30 +5,53 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h1>{{ $question->title}}</h1>
-                        <div class="ml-auto">
-                            <a href="{{ route('questions.index')}}" class="btn btn-outline-secondary">Back to All Questions</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">  
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Asked {{$question->created_date}}</span>
-                        <div class="media mt-2">
-                            <a href="{{$question->user->url}}" class='pr-2'>
-                                <img src="{{$question->user->avatar}}" alt="">
-                            </a>
-                            <div class="media-body mt-1">
-                                <a href="{{$question->user->url}}">
-                                    {{$question->user->name}}
-                                </a>
+                <div class="card-body">
+                    <div class="card-Title">
+                        <div class="d-flex align-items-center">
+                            <h1>{{ $question->title}}</h1>
+                            <div class="ml-auto">
+                                <a href="{{ route('questions.index')}}" class="btn btn-outline-secondary">Back to All Questions</a>
                             </div>
                         </div>
                     </div>
+                    <hr>
+                    <div class="media">  
+                        <div class="d-flex flex-column vote-controls">
+                            <a href="#" title="This is an important question" class="vote-up">
+                               <i class="fas fa-caret-up fa-2x"></i>
+                                <span class="votes-count">1234</span>
+                            </a>
+                           
+                            <a href="#" title="This question is not important" class="votep-down off">
+                                <i class="fas fa-caret-down fa-2x"></i>
+                            </a>
+
+                            <a href="#" title="Click to Mark as Favorite" class='favorite mt-2 favorited'>
+                                <i class="fas fa-star fa-lg"></i>
+                                <span class="favorites-count">254</span>
+                            </a>
+                        </div>
+
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Asked {{$question->created_date}}</span>
+                                <div class="media mt-2">
+                                    <a href="{{$question->user->url}}" class='pr-2'>
+                                        <img src="{{$question->user->avatar}}" alt="">
+                                    </a>
+                                    <div class="media-body mt-1">
+                                        <a href="{{$question->user->url}}">
+                                            {{$question->user->name}}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
+               
             </div>
         </div>
     </div>
@@ -42,6 +65,20 @@
                     <hr>
                     @foreach($question->answers as $answer)
                         <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a href="#" title="This is an important answer" class="vote-up">
+                                   <i class="fas fa-caret-up fa-2x"></i>
+                                    <span class="votes-count">1234</span>
+                                </a>
+                               
+                                <a href="#" title="This answer is not important" class="votep-down off">
+                                    <i class="fas fa-caret-down fa-2x"></i>
+                                </a>
+    
+                                <a href="#" title="Mark as Best answer" class='favorite mt-2 vote-accepted'>
+                                    <i class="fas fa-check fa-lg"></i>
+                                </a>
+                            </div>
                             <div class="media-body">
                                 {!! $answer->body_html !!}
                                 <div class="float-right">
